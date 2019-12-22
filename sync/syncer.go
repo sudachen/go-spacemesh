7a3a46cda3fc171ba9a925e742739cef38337324
almog <almogdepaz@gmail.com>
2019-12-22 15:02:37 +0200

    rename

--- sync/syncer.go
+++ sync/syncer.go
@@ -288,8 +288,9 @@ func (s *Syncer) Synchronise() {
 
 		lyr, err := s.GetLayer(currentSyncLayer)
 		if err != nil {
-			s.Panic("failed getting layer even though we are weakly-synced currentLayer=%v lastTicked=%v err=%v ", currentSyncLayer, s.lastTickedLayer(), err)
+			//s.Panic("failed getting layer even though we are weakly-synced currentLayer=%v lastTicked=%v err=%v ", currentSyncLayer, s.lastTickedLayer(), err)
 			return
+
 		}
 		s.lValidator.ValidateLayer(lyr) // wait for layer validation
 		return
