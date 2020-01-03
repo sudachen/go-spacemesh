274d608c93341997ce445f4e3ccd03bca8516e5c
barakiwi <barakiwi@barakiwis-MacBook-Pro.local>
2020-01-03 16:19:29 +0200

    comment in persist

--- mesh/mesh.go
+++ mesh/mesh.go
@@ -169,7 +169,7 @@ func (m *Mesh) ValidateLayer(lyr *types.Layer) {
 
 	oldPbase, newPbase := m.HandleIncomingLayer(lyr)
 	//oldPbase, newPbase := lyr.Index()-2, lyr.Index()-1
-	//m.Info("old_pbase, new_pbase: %v, %v", oldPbase, newPbase)
+	m.Info("old_pbase, new_pbase: %v, %v", oldPbase, newPbase)
 	m.lvMutex.Lock()
 	m.validatedLayer = lyr.Index()
 	if err := m.general.Put(VALIDATED, lyr.Index().ToBytes()); err != nil {
